60864f65add81fa9371a9332c8a8e1e8a0bb91b3
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2020-08-22 15:27:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# import libraries
-import urllib
+import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = [
@@ -7,25 +6,21 @@ BASE_URL = [
 'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-short-squat-stand-with-spotter-arms/400422.html',
 'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-tall-squat-stand-with-pull-up-bar-and-spotter-arms/400404.html',
 'https://www.titan.fitness/racks/squat-stands/t-3-series/t-3-series-short-squat-stand-with-j-hooks/400925.2.html',
-'https://www.titan.fitness/strength/barbells/olympic/regular-bar-20kg---chrome/430086.html'
+'https://www.titan.fitness/strength/barbells/olympic/regular-bar-20kg---chrome/430086.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/cast-iron-olympic-weight-plates-%7C-245-lb-set/430230.html'
 ]
-
-# loading empty array
-data = []
 
 # loop through the URLs we loaded above
 for pg in BASE_URL:
 	# query the website and return the html to the variable 'page'
-	page = urllib.request.urlopen(pg)
+	page = requests.get(pg)
 
 	# parse the html using Beautiful Soup and store in variable 'soup'
-	soup = BeautifulSoup(page, 'html.parser')
+	soup = BeautifulSoup(page.content, 'html.parser')
 
 	# take out the <div> of name and get its value
-	product_name_box = soup.find('span', attrs={'class': 'h1 product-name'})
-	product_name = product_name_box.text.strip()
-	print (product_name)
+	product_name = soup.find('span', attrs={'class': 'h1 product-name'})
+	print (product_name.text.strip())
 
-	availability_box = soup.find('span', attrs={'class': 'availability-msg'})
-	availability = availability_box.text.strip()
-	print (availability)
+	availability = soup.find('span', attrs={'class': 'availability-msg'})
+	print (availability.text.strip())
