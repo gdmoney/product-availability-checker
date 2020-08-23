899f1b5059a080020c4bfc953d0b34613c50eb25
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2020-08-22 18:18:30 -0700

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,34 @@
+import requests
+import yagmail
+from bs4 import BeautifulSoup
+
+BASE_URL = [
+'https://www.titan.fitness/racks/power-racks/x-3-series/x-3-half-rack/400406.html',
+'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-short-squat-stand-with-spotter-arms/400422.html',
+'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-tall-squat-stand-with-pull-up-bar-and-spotter-arms/400404.html',
+'https://www.titan.fitness/racks/squat-stands/t-3-series/t-3-series-short-squat-stand-with-j-hooks/400925.2.html',
+'https://www.titan.fitness/strength/barbells/olympic/regular-bar-20kg---chrome/430086.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/cast-iron-olympic-weight-plates-%7C-245-lb-set/430230.html'
+]
+
+# loop through the URLs we loaded above
+for pg in BASE_URL:
+	# query the website and return the html to the variable 'page'
+	page = requests.get(pg)
+
+	# parse the html using Beautiful Soup and store in variable 'soup'
+	soup = BeautifulSoup(page.content, 'html.parser')
+
+	# take out the <div> of name and get its value
+	product_name_box = soup.find('span', attrs={'class': 'h1 product-name'})
+	product_name = (product_name_box.text.strip())
+
+	availability_box = soup.find('span', attrs={'class': 'availability-msg'})
+	availability = (availability_box.text.strip())
+
+	status = product_name + " - " + availability
+	print (status)
+
+with open ('titan.txt', "w") as txt_file:
+	txt_file.write(status)
+	txt_file.close()
