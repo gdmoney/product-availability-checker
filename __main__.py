9fc0644aef1c47c78621a38f5a43d28501233a30
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2020-08-29 12:47:04 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -40,6 +40,7 @@ def send_email():
 	yag = yagmail.SMTP(FROM, 'hkxzacjexgundssc')
 	yag.send(TO, subject, contents)
 
+# email if one of the  products is available
 with open('titan.txt') as f:
     if 'In Stock' or 'Backorder' in f.read():
         send_email()
