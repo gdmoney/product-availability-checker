d09cc9f57cc20f4a0d8b492795caf88d04e81474
George Davitiani <George_Davitiani@intuit.com>
2020-09-19 15:38:17 -0700

    1

--- __main__.py
+++ __main__.py
@@ -1,12 +1,16 @@
+# pip install boto3
 # pip install beautifulsoup4
-# pip install keyring
 # pip install requests
-# pip install yagmail
 
+import boto3
+import email.utils
 import os
 import requests
-import yagmail
+import smtplib  
 from bs4 import BeautifulSoup
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
 
 BASE_URL = [
 'https://www.titan.fitness/racks/power-racks/x-3-series/x-3-half-rack/400406.html',
@@ -19,7 +23,8 @@ BASE_URL = [
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-100-lb-black-rubber-coated-hex-dumbbells/421101.html',
 'https://www.titan.fitness/racks/bench-press-rack-with-flip-down-safeties/400597.html',
 'https://www.titan.fitness/strength/barbells/olympic/atlas-bar---mens-20kg-barbell/430090.html',
-'https://www.titan.fitness/racks/power-racks/t-3-series/t-3-series-short-space-saving-racks/SSRT3SHUP-SSRT3.html'
+'https://www.titan.fitness/racks/power-racks/t-3-series/t-3-series-short-space-saving-racks/SSRT3SHUP-SSRT3.html',
+'https://www.titan.fitness/strength/specialty-machines/lower-body/economy-h-pnd/401027.html'
 ]
 
 # loop through the URLs above
@@ -44,12 +49,61 @@ for page in BASE_URL:
         f.write(status + '\n' + '\n')
 
 def send_email():
-    FROM = 'george.davitiani@gmail.com'
-    TO = 'george.davitiani@gmail.com'
-    subject = 'Titan Fitness'
-    contents = 'titan.txt'
-    yag = yagmail.SMTP(FROM, oauth2_file='~/oauth2_creds.json')
-    yag.send(TO, subject, contents)
+    CHARSET = 'utf-8'
+    AWS_REGION = 'us-west-2'
+    SENDER = 'george.davitiani@gmail.com'
+    RECIPIENT = 'george.davitiani@gmail.com'
+    SUBJECT = 'Titan Fitness Inventory Report'
+    ATTACHMENT = 'titan.txt'
+    BODY_TEXT = ('email body text.')
+    BODY_HTML = """<html>
+        <head></head>
+        <body>
+            <h1> email headline </h1>
+            <p> email body text. </p>
+        </body>
+        </html>"""
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    # Create a multipart/mixed parent container.
+    msg = MIMEMultipart('mixed')
+    # Add subject, from and to lines.
+    msg['Subject'] = SUBJECT 
+    msg['From'] = SENDER 
+    msg['To'] = RECIPIENT
+
+    # Create a multipart/alternative child container.
+    msg_body = MIMEMultipart('alternative')
+
+    # Encode the text and HTML content and set the character encoding. This step is
+    # necessary if you're sending a message with characters outside the ASCII range.
+    textpart = MIMEText(BODY_TEXT.encode(CHARSET), 'plain', CHARSET)
+    htmlpart = MIMEText(BODY_HTML.encode(CHARSET), 'html', CHARSET)
+
+    # Add the text and HTML parts to the child container.
+    msg_body.attach(textpart)
+    msg_body.attach(htmlpart)
+
+    # Define the attachment part and encode it using MIMEApplication.
+    att = MIMEApplication(open(ATTACHMENT, 'rb').read())
+
+    # Add a header to tell the email client to treat this part as an attachment, and to give the attachment a name.
+    att.add_header('Content-Disposition','attachment',filename=os.path.basename(ATTACHMENT))
+
+    # Attach the multipart/alternative child container to the multipart/mixed
+    # parent container.
+    msg.attach(msg_body)
+
+    # Add the attachment to the parent container.
+    msg.attach(att)
+
+    response = client.send_raw_email(
+        Source=SENDER,
+        Destinations=[RECIPIENT],
+        RawMessage={'Data':msg.as_string(),},
+    )
 
 # email if keywords are found in the text file
 with open('titan.txt') as f:
