88889bd125c606e89d1af1e8717a47c3b115119f
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2020-10-06 13:02:26 -0700

    Update lambda_function.py

--- AWS/lambda_function.py
+++ AWS/lambda_function.py
@@ -9,17 +9,15 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 BASE_URL = [
-'https://www.titan.fitness/racks/power-racks/x-3-series/x-3-half-rack/400406.html',
-'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-short-squat-stand-with-spotter-arms/400422.html',
-'https://www.titan.fitness/racks/squat-stands/x-3-series/x-3-series-tall-squat-stand-with-pull-up-bar-and-spotter-arms/400404.html',
-'https://www.titan.fitness/racks/squat-stands/t-3-series/t-3-series-short-squat-stand-with-j-hooks/400925.2.html',
-'https://www.titan.fitness/racks/squat-stands/t-3-series/titan-t-3-series-squat-stand-v2/400994.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/230-lb-set-economy-black-bumper-plates/430117.html',
+'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/45-lb-single-cast-iron-olympic-plates/430232.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/10-lb-pair-cast-iron-olympic-plates/430210.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/5-lb-pair-cast-iron-olympic-plates/430209.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-75-lb-black-rubber-coated-hex-dumbbells/421076.html',
-'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-100-lb-black-rubber-coated-hex-dumbbells/421101.html',
-'https://www.titan.fitness/racks/bench-press-rack-with-flip-down-safeties/400597.html',
-'https://www.titan.fitness/strength/barbells/olympic/atlas-bar---mens-20kg-barbell/430090.html',
-'https://www.titan.fitness/racks/power-racks/t-3-series/t-3-series-short-space-saving-racks/SSRT3SHUP-SSRT3.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-85-lb-black-rubber-coated-hex-dumbbells/421086.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-90-lb-black-rubber-coated-hex-dumbbells/421091.html',
+'https://www.titan.fitness/strength/dumbbells/neoprene/neoprene-light-weight-dumbbell-set---1%2C-2%2C-3-%2C-4-lb/420003.html',
 'https://www.titan.fitness/strength/specialty-machines/lower-body/economy-h-pnd/401027.html'
 ]
 
