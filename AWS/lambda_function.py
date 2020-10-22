951a1ed4943682fe42f34b24e31473889c9c1e32
George Davitiani <george.davitiani@gmail.com>
2020-10-22 14:59:28 -0700

    Update lambda_function.py

--- AWS/lambda_function.py
+++ AWS/lambda_function.py
@@ -11,6 +11,7 @@ from email.mime.application import MIMEApplication
 BASE_URL = [
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/230-lb-set-economy-black-bumper-plates/430117.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
+'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/25-lb-pair-economy-black-bumper-plates/430103.2.html',
 'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-spotter-arms/400413.html',
 'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-dual-pull-up-stabilizer-bar/400307.html',
