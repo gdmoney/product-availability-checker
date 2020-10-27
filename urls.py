038506d251c5970b5bc05cfbbfaa7c2655c4463d
George Davitiani <george.davitiani@gmail.com>
2020-10-27 11:07:51 -0700

    created urls.py module
    to separate the url list from the code

---
+++ urls.py
@@ -0,0 +1,8 @@
+URL_LIST = [
+'https://www.titan.fitness/strength/weight-plates/bumper-plates/230-lb-set-economy-black-bumper-plates/430117.html',
+'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
+'https://www.titan.fitness/strength/weight-plates/bumper-plates/25-lb-pair-economy-black-bumper-plates/430103.2.html',
+'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-spotter-arms/400413.html',
+'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-dual-pull-up-stabilizer-bar/400307.html',
+'https://www.titan.fitness/racks/rack-accessories/t-3-series/olympic-weight-plate-holder-for-t-3-power-rack-3-inchx2-inch-tube/400116.html'
+]
