0fea0be86949ae8c83a372656a0c1c27ab68d2ed
George Davitiani <george.davitiani@gmail.com>
2020-10-30 10:40:24 -0700

    updated the url list

--- AWS/urls.py
+++ AWS/urls.py
@@ -2,7 +2,9 @@ URL_LIST = [
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/230-lb-set-economy-black-bumper-plates/430117.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/25-lb-pair-economy-black-bumper-plates/430103.2.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/25-lb-pair-cast-iron-olympic-plates/430211.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/10-lb-pair-cast-iron-olympic-plates/430210.html',
+'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/5-lb-pair-cast-iron-olympic-plates/430209.html',
 'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-spotter-arms/400413.html',
-'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-dual-pull-up-stabilizer-bar/400307.html',
-'https://www.titan.fitness/racks/rack-accessories/t-3-series/olympic-weight-plate-holder-for-t-3-power-rack-3-inchx2-inch-tube/400116.html'
+'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-dual-pull-up-stabilizer-bar/400307.html'
 ]
