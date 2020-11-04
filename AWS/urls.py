7d24567255ccee81f49313f600072f2044840407
George Davitiani <george.davitiani@gmail.com>
2020-11-04 08:20:37 -0800

    updated URL lists

--- AWS/urls.py
+++ AWS/urls.py
@@ -5,6 +5,5 @@ URL_LIST = [
 'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/25-lb-pair-cast-iron-olympic-plates/430211.html',
 'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/10-lb-pair-cast-iron-olympic-plates/430210.html',
 'https://www.titan.fitness/strength/weight-plates/cast-iron-plates/5-lb-pair-cast-iron-olympic-plates/430209.html',
-'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-spotter-arms/400413.html',
-'https://www.titan.fitness/racks/rack-accessories/t-3-series/t-3-series-dual-pull-up-stabilizer-bar/400307.html'
+'https://www.titan.fitness/strength/barbells/accessories/mini-bar-jack-set-of-2/430019.html'
 ]
