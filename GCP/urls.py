c6f584463d68e2d88278c94b5b75086823d01279
George Davitiani <george.davitiani@gmail.com>
2020-12-04 09:52:49 -0800

    updated url list

--- GCP/urls.py
+++ GCP/urls.py
@@ -1,9 +1,12 @@
 URL_LIST = [
-'https://www.titan.fitness/strength/weight-plates/bumper-plates/230-lb-set-economy-black-bumper-plates/430117.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/45-lb-single-economy-black-bumper-plate/430105.2.html',
 'https://www.titan.fitness/strength/weight-plates/bumper-plates/25-lb-pair-economy-black-bumper-plates/430103.2.html',
-'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-75-lb-black-rubber-coated-hex-dumbbells/421076.html',
-'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/pair-of-100-lb-black-rubber-coated-hex-dumbbells/421101.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/75-lb-rubber-hex-dumbbells/421076.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/80-lb-rubber-hex-dumbbells/421081.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/85-lb-rubber-hex-dumbbells/421086.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/90-lb-rubber-hex-dumbbells/421091.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/95-lb-rubber-hex-dumbbells/421096.html',
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/100-lb-rubber-hex-dumbbells/421101.html',
 'https://www.titan.fitness/strength/barbells/accessories/mini-bar-jack-set-of-2/430019.html',
 'https://www.titan.fitness/strength/specialty-machines/lower-body/indoor-tire-flipping-machine/401136.html'
 ]
