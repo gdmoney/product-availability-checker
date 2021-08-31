72766c0a7648b685a0fd61893d79042605832d7c
George Davitiani <george.davitiani@gmail.com>
2021-08-31 09:00:20 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,6 +4,5 @@ URL_LIST = [
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/85-lb-rubber-hex-dumbbells/421086.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/90-lb-rubber-hex-dumbbells/421091.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/95-lb-rubber-hex-dumbbells/421096.html',
-'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/100-lb-rubber-hex-dumbbells/421101.html',
-'https://www.titan.fitness/strength/specialty-machines/lower-body/indoor-tire-flipping-machine/401136.html'
+'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/100-lb-rubber-hex-dumbbells/421101.html'
 ]
