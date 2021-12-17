d43d4545b3467e9f1cddf3d05172a49e0b50a661
gdmoney <46586741+gdmoney@users.noreply.github.com>
2021-12-17 12:11:16 -0800

    exception handling test

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ URL_LIST = [
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/80-lb-rubber-hex-dumbbells/421192.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/85-lb-rubber-hex-dumbbells/421193.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/90-lb-rubber-hex-dumbbells/421194.html',
+'https://www.titan.fitness/blahblahblah',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/95-lb-rubber-hex-dumbbells/421195.html',
 'https://www.titan.fitness/strength/dumbbells/rubber-coated-hex/100-lb-rubber-hex-dumbbells/421196.html',
 'https://www.titan.fitness/strength/specialty-machines/lower-body/indoor-tire-flipping-machine/401136.html'
